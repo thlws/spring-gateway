f4af420387850353448efdcd41344e52728d96ce
Hanley <hanley@thlws.com>
2020-08-08 23:18:13 +0800

    分页请求规范

--- src/main/java/com/thlws/springcloud/gateway/data/PageResult.java
+++ src/main/java/com/thlws/springcloud/gateway/data/PageResult.java
@@ -16,7 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 public class PageResult<T> implements Serializable {
 
-    private List<T> records = Collections.emptyList();
+    private List<T> list = Collections.emptyList();
 
     private long total;
 
